636fa8c8d62ea0f0bb667390fc5e3ad0e29a62e5
Michael Abebe <mtabebe@uwaterloo.ca>
2018-09-07 14:23:02 -0400

    Add bad writing script

---
+++ detectBadWriting.sh
@@ -0,0 +1,12 @@
+filename="$1"
+path=$( dirname "$0" )
+echo $path
+
+echo " --- Duplicates --- "
+perl $path/duplicates.pl $filename
+
+echo " --- Weasel Words --- "
+$path/weasel.sh $filename
+
+echo " --- Passive Voice --- "
+$path/passive.sh $filename
